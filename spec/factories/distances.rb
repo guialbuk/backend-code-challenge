2f4f2b73236df0d41491cfb5c2703c6737b4a0d0
Gui Vieira <guilherme@n-pix.com>
2018-04-02 16:37:32 -0300

    Create Distance validations

--- spec/factories/distances.rb
+++ spec/factories/distances.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :distance do
-    origin { ('A'..'Z').to_a.sample }
-    destination { (('A'..'Z').reject { |a| a == origin }).sample }
+    origin { ('A'..'Z').to_a[0..-2].sample }
+    destination 'Z'
     length { rand(1..100000) }
   end
 end
