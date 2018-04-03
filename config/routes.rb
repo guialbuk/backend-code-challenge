002aabda7bee5bbb912b0d9b4e2a20155855becd
Gui Vieira <guilherme@n-pix.com>
2018-04-03 20:43:21 -0300

    Disable root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: proc { [404, {}, ['404 (Not Found)']] }
+
   post '/distance', to: 'distances#create', as: 'distance'
   get '/cost', to: 'costs#show', as: 'cost'
 end
